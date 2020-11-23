8d8d0a2e364f838386dcc4cc26356712a2771cc9
Muhammad Ubaid Raza <mubaidr@gmail.com>
2020-11-23 21:43:26 +0500

    fix test

--- test/full.test.ts
+++ test/full.test.ts
@@ -89,16 +89,16 @@ describe('ContourFinder', () => {
 
     expect(found.length).toBe(2)
     expect(found[0]).toEqual([
+      { x: 0, y: 1 },
       { x: 0, y: 0 },
       { x: 1, y: 0 },
       { x: 1, y: 1 },
-      { x: 0, y: 1 },
     ])
     expect(found[1]).toEqual([
+      { x: 3, y: 2 },
       { x: 3, y: 1 },
       { x: 4, y: 1 },
       { x: 4, y: 2 },
-      { x: 3, y: 2 },
     ])
   })
 

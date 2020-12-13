9058bc1e9faafee9b0bd0996f2764ccb9941b946
Muhammad Ubaid Raza <mubaidr@gmail.com>
2020-12-13 20:18:49 +0500

    improve shape types

--- src/types/ShapeType.ts
+++ src/types/ShapeType.ts
@@ -1,6 +1,17 @@
-export enum ShapeTypes {
-  Rectangle,
-  Circle,
+export interface Point {
+  x: number
+  y: number
+}
+
+export interface Line {
+  P1: Point
+  P2: Point
+}
+
+export interface Triangle {
+  P1: Point
+  P2: Point
+  P3: Point
 }
 
 export interface Rectangle {
@@ -14,4 +25,15 @@ export interface Circle {
   x: number
   y: number
   radius: number
+}
+
+export interface Polygon extends Array<Point> {}
+
+export interface ShapeCollection {
+  points: Point[]
+  lines: Line[]
+  triangles: Triangle[]
+  recangles: Rectangle[]
+  circles: Circle[]
+  polygons: Polygon[]
 }
